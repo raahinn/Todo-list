9879c3ca72017232231a6d329194babc09240ad9
raahinn <rahinn11@gmail.com>
2020-09-03 21:01:14 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+`;
+
+    list.innerHTML += html;
+
+};
+
+// add items in todo list
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if (todo.length) {
+        generateTemplate(todo);
+        addForm.reset();
+    };
+
+});
+
+
